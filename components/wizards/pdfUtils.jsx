7055f9dd44f4b5d16597b8cd7cf75288770cd180
Vinayaka Pai Kutkunje <paivinayak187@gmail.com>
2023-05-16 17:03:34 +0530

    Added pdf css

--- components/wizards/pdfUtils.jsx
+++ components/wizards/pdfUtils.jsx
@@ -1,23 +1,46 @@
-import { Page, Text, View, Document, Path, StyleSheet } from '@react-pdf/renderer';
+import { Page, Text, Svg, Document, Rect, StyleSheet } from '@react-pdf/renderer';
 
 // Create PDF styles
 const styles = StyleSheet.create({
-    container: {
-        marginBottom: 10,
+    page: {
+        padding: 60,
+    },
+    rect: {
+        stroke: "black",
+        rx: "5",
+        ry: "5"
     }
 });
 
 export const PDFDocument = ({ state }) => (
     <Document>
-        <Page>
-            <View>
-                <Path d="M50 50 H150 V100 H50 Z" stroke="black" />
-                <Text style={{ position: 'absolute', top: 75, left: 75 }}>
-                    Sample Text
-                </Text>
-                <Text>{`${state.ticket.number}`}</Text>
-                <Text>{`${state.ticket.title}`}</Text>
-            </View>
+        <Page style={styles.page} size="A4">
+            <Svg viewBox="0 0 190 200">
+                <>
+                    <Text style={{ x: 2, y: 7, fontSize: 7.5 }}>
+                        {`EDA Date: ${state.generic.edaDate}`}
+                    </Text>
+                </>
+                <>
+                    <Rect y="10" width="25%" height="10" style={styles.rect} />
+                    <Text style={{ x: 2, y: 17, fontSize: 7.5 }}>
+                        {`${state.ticket.number}`}
+                    </Text>
+                    <Rect x="55" y="10" width="70%" height="10" style={styles.rect} />
+                    <Text style={{ x: 60, y: 17, fontSize: 7.5 }}>
+                        {`${state.ticket.title}`}
+                    </Text>
+                    <Rect y="25" width="100%" height="60" style={styles.rect} />
+                    <Text style={{ x: 5, y: 35, fontSize: 6 }}>
+                        {`${state.ticket.description}`}
+                    </Text>
+                </>
+                <>
+                    <Text style={{ x: 2, y: 100, fontSize: 9 }}>
+                        EDA Report
+                    </Text>
+                </>
+            </Svg>
         </Page>
     </Document>
 );
